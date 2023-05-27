3ae4d8dbe1b321ed1e1fc3b252ac0a84a87df4f2
GeorgyFirsov <gfirsov007@gmail.com>
2023-05-27 13:54:59 +0300

    chore: installation script
    Signed-off-by: GeorgyFirsov <gfirsov007@gmail.com>

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+
+if [[ $# -ne 1 ]]; then
+    echo "Wrong number of arguments. Usage: ./install.sh </path/to/profile-file>" >&2
+    return 1
+fi
+
+CURRENT_DIR=$(dirname -- "$(readlink -f -- "$0";)";)
+SRC_DIR=$CURRENT_DIR/src
+BIBLATEX_DIR=$SRC_DIR/biblatex
+PROFILE_FILE=$1
+
+if [ ! -f $PROFILE_FILE ]; then
+    echo "Profile file doesn't exist"
+    return 1
+fi
+
+echo "[i] Profile file: $PROFILE_FILE" >&1
+echo "[i] LaTeX files: $SRC_DIR" >&1
+echo "[i] Biblatex files: $BIBLATEX_DIR" >&1
+
+#
+# Write header
+#
+
+echo "" >> $PROFILE_FILE
+echo "# Begin GOST LaTeX section" >> $PROFILE_FILE
+echo "#" >> $PROFILE_FILE
+
+#
+# Set variables
+#
+
+echo "[i] Setting environment variables" >&1
+
+echo "export TEXINPUTS=$SRC_DIR:$TEXINPUTS" >> $PROFILE_FILE
+echo "export BSTINPUTS=$BIBLATEX_DIR:$BSTINPUTS" >> $PROFILE_FILE
+
+echo "[+] Environment variables done" >&1
+
+#
+# Write trailer
+#
+
+echo "#" >> $PROFILE_FILE
+echo "# End GOST LaTex section" >> $PROFILE_FILE
+echo "" >> $PROFILE_FILE
+
+echo "[+] Installation completed. Please source your profile file or restart terminal" >&1
+
